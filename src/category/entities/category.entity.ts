a88460c0fe95ace1e18e77d2036db5a6bdbfd65e
huevangxp <huevang933@gmail.com>
2026-01-12 15:04:53 +0700

    commit Mon 01/12/2026 15:04:53.21

--- src/category/entities/category.entity.ts
+++ src/category/entities/category.entity.ts
@@ -1,1 +1,10 @@
-export class Category {}
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity()
+export class Category {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+}
